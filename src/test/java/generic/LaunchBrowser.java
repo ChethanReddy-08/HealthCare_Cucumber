b4534547eaaa446e02f4c05bd58f39ecb9ab5f87
Chethan <chethan2308@gmail.com>
2025-05-16 11:24:54 +0530

    added baseclass

--- src/test/java/generic/LaunchBrowser.java
+++ src/test/java/generic/LaunchBrowser.java
@@ -1,7 +1,33 @@
 package generic;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LaunchBrowser
 {
+	public WebDriver driver;
+	@BeforeMethod
+	public void launchBrowser()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://106.51.87.42:9007/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.HOURS);
+		
+	}
 	
-
+	
+	@AfterMethod
+	public void closeBrowser()
+	{
+		driver.close();
+	}
 }
